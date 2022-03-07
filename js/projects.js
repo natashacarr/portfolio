dbb2fc64b54c5dca7b3e560dfd64e4281aa18490
Natasha Carr <97845462+natashacarr@users.noreply.github.com>
2022-03-07 10:25:29 +0000

    snippet card css

--- js/projects.js
+++ js/projects.js
@@ -4,3 +4,10 @@ $(document).ready(function () {
     $("#boozer-header").toggleClass("active");
   });
 });
+
+$(document).ready(function () {
+  $("#snippet-header").click(function () {
+    $("#snippet-details").slideToggle();
+    $("#snippet-header").toggleClass("active");
+  });
+});
